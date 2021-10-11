0ad336b9f060e298cca79f5dd8b188a86228a9c7
Artur Stepanyants <alfacentaur@yandex.ru>
2021-10-11 11:21:33 +0300

    lesson_1

---
+++ lesson1/ListWrapper/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading;
+
+namespace ListWrapper
+{
+
+    class Program
+    {
+        private static readonly int _countOfListItems = 10;
+
+        private static ListWrapper<int> lst = new ListWrapper<int>();
+
+        static void Main(string[] args)
+        {
+
+            Thread threadAdd = new Thread(new ThreadStart(() => TestAdd(_countOfListItems)));
+
+            Thread threadRemove = new Thread(new ThreadStart(() => TestRemove(_countOfListItems)));
+
+            threadAdd.Start();
+
+            threadRemove.Start();
+
+            Thread.Sleep(2000);
+
+            Random r = new Random();
+
+            int rndValue = 0;
+
+            for (int i = 0; i < _countOfListItems; i++)
+            {
+                lst.Add(i);
+
+                Console.WriteLine("Поток Main добавил элемент: {0}", i);
+
+                PrintList();
+
+                rndValue = r.Next(10);
+
+                lst.Remove(rndValue);
+
+                Console.WriteLine("Поток Main пытался удалить элемент: {0}", rndValue);
+
+                PrintList();
+
+                Thread.Sleep(400);
+            }
+
+        }
+
+        private static void TestAdd(int countOfListItems)
+        {
+            for (int i = 0; i < countOfListItems; i++)
+            {
+                lst.Add(i);
+
+                Console.WriteLine("Поток TestAdd добавил элемент: {0}", i);
+
+                PrintList();
+
+                Thread.Sleep(400);
+            }
+        }
+
+        private static void TestRemove(int otem)
+        {
+            Random r = new Random();
+            int rndValue = 0;
+            for (int i = 0; i < _countOfListItems; i++)
+            {
+                Thread.Sleep(400);
+
+                rndValue = r.Next(10);
+
+                lst.Remove(rndValue);
+
+                Console.WriteLine("Поток TestRemove пытался удалить  элемент: {0}", rndValue);
+
+                PrintList();          
+            }                                      
+        }
+
+        private static void PrintList()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("Список: ");
+
+            for (int i = 0; i < lst.Count; i++)
+            {
+                Console.Write(String.Concat(lst[i], " "));
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ lesson1/WPF/Fibo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Threading;
+
+namespace lesson1
+{
+    public class Fibo
+    {
+        public static int fib(int n)
+        {
+            return n > 1 ? fib(n - 1) + fib(n - 2) : n;
+        }
+    }
+}

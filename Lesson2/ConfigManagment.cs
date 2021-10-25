3c029bffb57a246f825f39a12289b1a0eee90ca3
Artur Stepanyants <alfacentaur@yandex.ru>
2021-10-25 13:14:00 +0300

    Lesson2_mod2
    Никак не получилось у меня не создавать в пуле новый thread, т.к. нигде не нашел информации, как оставить поток живым чтобы он в завершенные не переходил и назначить ему новый Action на выполнение. Совет в инете был Thread на Task заменить. Я попробовал, но получилось ещё хуже, чем со thread.  Вот эти два вопроса оказались для меня неразрешимыми...

---
+++ Lesson2/ConfigManagment.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace Lesson2
+{
+    public class ConfigManagment
+    {
+        public ConfigManagment()
+        {
+
+        }
+
+        public string ReadSetting(string key)
+        {
+            string setting = null;
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                setting = appSettings[key];
+
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+            return setting;
+        }
+
+        public void AddUpdateAppSettings(object key, object value)
+        {
+            try
+            {
+                var _key = key.ToString();
+
+                var _value = value.ToString();
+
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                var settings = configFile.AppSettings.Settings;
+
+                if (settings[_key] == null)
+                {
+                    settings.Add(_key, _value);
+                }
+                else
+                {
+                    settings[_key].Value = _value;
+                }
+
+                configFile.Save(ConfigurationSaveMode.Modified);
+
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+            
+        public ICollection<KeyValuePair<string, string>> ReadAllSettings()
+        {
+            Dictionary<string, string> settings = new Dictionary<string, string>();
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                foreach(var p in appSettings.AllKeys)
+                {
+                    settings.Add(p, appSettings[p]);
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+            return settings;
+        }
+    }
+}

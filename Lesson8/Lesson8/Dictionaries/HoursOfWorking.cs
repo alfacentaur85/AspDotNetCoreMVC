25eaeb9df1bb331f15b892e175e2aebb9b96a4c6
Artur Stepanyants <alfacentaur@yandex.ru>
2022-01-23 18:18:06 +0300

    lesson8 mod 2

---
+++ Lesson8/Lesson8/Dictionaries/HoursOfWorking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson7.Dictionaries
+{
+    public static class HoursOfWorking
+    {
+        public static Dictionary<string, string> hoursOfWorking = new Dictionary<string, string>
+        {
+            { "OrdinaryDays", "9 - 18" },
+            { "Sunday", "Holyday" },
+            { "Friday", "9 - 17"},
+            { "Saturday", "10 - 16"}
+        };
+    }
+}
